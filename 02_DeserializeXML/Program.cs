e5c4f54eb767f17912db89b02ee46dc1e44ece9a
trtg <trtg10@gmail.com>
2018-07-10 20:56:00 -0300

    Deserialize XML

---
+++ 02_DeserializeXML/Program.cs
@@ -0,0 +1,22 @@
+﻿using _00_Library;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace _02_DeserializeXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"D:\Users\Thiago\Documents\01-SerializeXML.xml");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(User));
+            User user = (User)serializer.Deserialize(stream);
+
+            Console.WriteLine($"Name: {user.Name}, CPF: {user.CPF}, Email: {user.Email}");
+
+            Console.ReadKey();
+        }
+    }
+}
