11f65e4d51a76f38d30cca3ce97b1f61445f2034
trtg <trtg10@gmail.com>
2018-07-16 20:13:24 -0300

    Serialize/Deserialize JSON

---
+++ 03_SerializeJSON/Program.cs
@@ -0,0 +1,30 @@
+﻿using _00_Library;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace _03_SerializeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User {
+                Name = "Maria",
+                CPF = "333.333.333-33",
+                Email = "mail@mail.com"
+            };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            string stringJsonSerialize = serializer.Serialize(user);
+
+            StreamWriter sw = new StreamWriter(@"D: \Users\Thiago\Documents\02-SerializeJSON.json");
+            sw.WriteLine(stringJsonSerialize);
+
+            sw.Close();
+
+
+
+        }
+    }
+}
---
+++ 04_DeserializeJSON/Program.cs
@@ -0,0 +1,26 @@
+﻿using _00_Library;
+using System;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace _04_DeserializeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D: \Users\Thiago\Documents\02-SerializeJSON.json");
+            string fileLine = sr.ReadToEnd();
+
+            JavaScriptSerializer serialize = new JavaScriptSerializer();
+            serialize.Deserialize(fileLine, typeof(User));
+
+            User user = (User)serialize.Deserialize(fileLine, typeof(User));
+
+            Console.WriteLine($"Name: {user.Name} | CPF: {user.CPF} | Email: {user.Email}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
