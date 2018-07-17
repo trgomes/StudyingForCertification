19dd91f5576ef203ed4f32dd0dc31c65023c7d87
trtg <trtg10@gmail.com>
2018-07-17 20:51:38 -0300

    Extension Methods

---
+++ 02_ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "olá mundo";
+            Console.WriteLine(str.FirstToUpper());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_ExtensionMethods/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static string FirstToUpper( this String str)
+        {
+            string first = str.Substring(0,1);
+            string end = str.Substring(1);
+
+            return first.ToUpper() + end;
+        }
+    }
+}
