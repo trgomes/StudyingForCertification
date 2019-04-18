960ee2d234cbc4584e0b84789f37b2b144e509e2
Thiago Gomes <thiago.gomes@saipher.com.br>
2019-04-18 10:18:24 -0300

    Thread pool

---
+++ 7-Thread-Pool/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _7_Thread_Pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch mywatch = new Stopwatch();
+
+            Console.WriteLine("Thread Pool Execution");
+
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+
+
+            Console.WriteLine("Thread Execution");
+
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+
+            Console.ReadKey();
+        }
+
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Process));
+            }
+        }
+
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread o = new Thread(Process);
+                o.Start();
+            }
+        }
+
+        static void Process(object callback)
+        {
+            Console.WriteLine("Pipoco");
+        }
+    }
+}
